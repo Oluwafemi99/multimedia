d54eb25488e505db5dd1b51024f4bdd452f5762e
oluwafemi <oluwafemidojumo@gmail.com>
2026-03-23 08:12:26 +0100

    secret_key refix

--- multimedia/multimedia/settings.py
+++ multimedia/multimedia/settings.py
@@ -194,7 +194,7 @@ AUTHENTICATION_BACKENDS = [
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": os.getenv("REDIS_URL", "redis://127.0.0.1:6379/1"),
+        "LOCATION": "redis://127.0.0.1:6379/1",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
